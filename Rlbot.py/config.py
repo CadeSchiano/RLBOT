3710b3158de8f01765f96df7dc6cfa98e97e3d43
CadeSchiano <cadeschiano8@yahoo.com>
2025-12-02 03:48:55 +0000

    Updated config.py

--- Rlbot.py/config.py
+++ Rlbot.py/config.py
@@ -24,11 +24,13 @@ if dotenv_path.exists():
                         value = value[1:-1]
                     # Set in environment
                     os.environ[key] = value
-DISCORD_TOKEN = os.getenv('MTQwOTIzMDY4NTU3MDQ2OTg5MQ.GL9gW3.PostVBAwltdkQYIiG-SLyfYimCqBwvzFeuVHzY')
 
+
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 # RapidAPI Key for Rocket League API
-RAPIDAPI_KEY = os.getenv('401899db29msh9edc353bdb9bd39p1b5581jsn38884637372b')
+RAPIDAPI_KEY = os.getenv('RAPIDAPI_KEY')
 
 RAPIDAPI_HOST = "rocket-league1.p.rapidapi.com"
 
